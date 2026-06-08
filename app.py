0b61b74c9a4ab39d5818b6e5c3c6e19adc186019
Dhabia Luiza Ramos Inacio <1117793@senacminas.edu.br>
2026-06-08 14:12:36 -0300

    Configuração Railway

--- app.py
+++ app.py
@@ -450,5 +450,10 @@ def arquivar_pedido(pedido_id):
 # INICIAR SERVIDOR
 # =========================
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
